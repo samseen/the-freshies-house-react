bea1601a5f24fe8a6d8d8b8829c3091021cdf526
Samson Akanbi <samson.akanbi@outlook.com>
2021-03-04 08:26:41 +0100

    Created the Product component.

--- src/components/Navbar/Products/Products.jsx
+++ src/components/Navbar/Products/Products.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import Grid from '@material-ui/core';
 
 const products = [
-    { id: 1, name: 'Orange Juice', description: 'Sweet Orange' },
-    { id: 2, name: 'Apple Juice', description: 'Sweet Apple' },
+    { id: 1, name: 'Orange Juice', description: 'Sweet Orange', price: 'NGN 500' },
+    { id: 2, name: 'Apple Juice', description: 'Sweet Apple', price: 'NGN 500' },
 ];
 
 const Products = () => {
     return (
         <main>
             <Grid container justify="center" spacing={4}>
-                {products.map(product) => (
+                {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product />
+                        <Product product={product}/>
                     </Grid>
-                )}
+                ))}
             </Grid>
         </main>
     )
