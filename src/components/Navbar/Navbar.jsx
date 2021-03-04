bd956cd773b62d09baf05be02411837005c056cf
Samson Akanbi <samson.akanbi@outlook.com>
2021-03-04 07:24:54 +0100

    Created the Products component.

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+
+function Navbar() {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Navbar
