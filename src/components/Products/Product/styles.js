6182cf3df75c8e337741fdc23f9b64ed441c23a0
Samson Akanbi <samson.akanbi@outlook.com>
2021-03-09 18:25:00 +0100

    Displayed selected cart items in the console.

--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -7,6 +7,7 @@ export default makeStyles(() => ({
     media: {
         height: 0,
         paddingTop: '56.25%', //16:9
+        //paddingTop: '100%', //16:9
     },
     cardActions: {
         display: 'flex',
