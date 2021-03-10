d8a1dc138eb32031f45176c033690dd1224b02cb
Samson Akanbi <samson.akanbi@outlook.com>
2021-03-10 09:38:05 +0100

    Displayed the selected items in a card.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 
 import useStyles from './styles';
+import CartItem from './CartItem/CartItem';
 
 const Cart = ({ cart }) => {
-    const isEmpty = !cart.line_items.length;
-    // const isEmpty = !0;
     const classes = useStyles();
 
     const EmptyCart = () => (
@@ -17,7 +16,7 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <div>{item.name}</div>
+                        <CartItem item={item} />
                     </Grid>
                 ))}
             </Grid>
@@ -29,13 +28,15 @@ const Cart = ({ cart }) => {
                 </div>
             </div>
         </>
-    )
+    );
+
+    if(!cart.line_items) return 'Loading...';
 
     return (
         <Container>
             <div className={classes.toolbar} />
             <Typography className={classes.title} variant="h3">Your Juice Basket</Typography>
-            { isEmpty ? <EmptyCart /> : <FilledCart /> }
+            { !cart.line_items ? <EmptyCart /> : <FilledCart /> }
         </Container>
     )
 }
