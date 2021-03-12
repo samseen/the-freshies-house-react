22a5f3672c4f4fa0f19f655270381b1ae36529d6
Samson Akanbi <samson.akanbi@outlook.com>
2021-03-12 19:31:17 +0100

    Laid out the templates for the check out transitions.

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -2,12 +2,22 @@ import React, { useState } from 'react';
 import  { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 
 import useStyles from './styles';
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
 
 const steps = ['Shipping Address', 'Payment Details'];
 
 const Checkout = () => {
-    const [activeStep, setActiveStep] = useState(0);
+    const [activeStep, setActiveStep] = useState(3);
     const classes = useStyles();
+
+    const Confirmation = () => (
+        <div>Confirmation</div>
+    )
+
+    const Form = () => activeStep === 0
+        ? <AddressForm />
+        : <PaymentForm />
 
     return (
         <>
@@ -15,13 +25,14 @@ const Checkout = () => {
             <main className={classes.layout}>
                 <Paper className={classes.paper}>
                     <Typography variant="h4" align="center">Checkout</Typography>
-                    <Stepper activeStep={0} className={classes.stepper}>
+                    <Stepper activeStep={activeStep} className={classes.stepper}>
                         {steps.map((step) => (
                             <Step key={step}>
                                 <StepLabel>{step}</StepLabel>
                             </Step>
                         ))}
                     </Stepper>
+                    {activeStep === steps.length ? <Confirmation /> : <Form />}
                 </Paper>
             </main>
         </>
